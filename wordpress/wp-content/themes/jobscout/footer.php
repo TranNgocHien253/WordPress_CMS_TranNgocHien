3d344a88dc9c5dffb2bbcc6828dbb6a4651bd9d9
TranNgocHien253 <161793880+TranNgocHien253@users.noreply.github.com>
2024-10-18 17:19:05 +0700

    comt

--- wordpress/wp-content/themes/jobscout/footer.php
+++ wordpress/wp-content/themes/jobscout/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,109 +9,107 @@
  *
  * @package JobScout
  */
-    
-    /**
-     * After Content
-     * 
-     * @hooked jobscout_content_end - 20
-    */
-    do_action( 'jobscout_before_footer' );
-    
-    /**
-     * Footer
-     * 
-     * @hooked jobscout_footer_start  - 20
-     * @hooked jobscout_footer_top    - 30
-     * @hooked jobscout_footer_bottom - 40
-     * @hooked jobscout_footer_end    - 50
-    */
-    do_action( 'jobscout_footer' );
-    
-    // /**
-    //  * After Footer
-    //  * 
-    //  * @hooked jobscout_page_end    - 20
-    // */
-    // do_action( 'jobscout_after_footer' );
 
-    // wp_footer(); 
-	$categories = get_categories();
-foreach ($categories as $category) {
-    // echo $category->term_id . ' - ' . $category->name . ' - ' . $category->slug . '<br>';
-	}
+/**
+ * After Content
+ * 
+ * @hooked jobscout_content_end - 20
+ */
+do_action('jobscout_before_footer');
 
-	?>
+/**
+ * Footer
+ * 
+ * @hooked jobscout_footer_start  - 20
+ * @hooked jobscout_footer_top    - 30
+ * @hooked jobscout_footer_bottom - 40
+ * @hooked jobscout_footer_end    - 50
+ */
+do_action('jobscout_footer');
 
-</body>
+// /**
+//  * After Footer
+//  * 
+//  * @hooked jobscout_page_end    - 20
+// */
+// do_action( 'jobscout_after_footer' );
+
+wp_footer();
+$categories = get_categories();
+// foreach ($categories as $category) {
+//     // echo $category->term_id . ' - ' . $category->name . ' - ' . $category->slug . '<br>';
+// 	}
+
+?>
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="/./wp-content/./themes/./jobscout/./footer.css">
 <!------ Include the above in your HEAD tag ---------->
 
 <!-- Footer -->
-	<section id="footer">
-		<div class="container">
-			<div class="row text-center text-xs-center text-sm-left text-md-left">
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
+<section id="footer">
+	<div class="container">
+		<div class="row text-center text-xs-center text-sm-left text-md-left">
+			<div class="col-xs-12 col-sm-4 col-md-4">
+				<h5>Quick links</h5>
+				<ul class="list-unstyled quick-links">
 
-						
-						<?php
-                        foreach ($categories as $category) {
-                            // echo $category->term_id . ' - ' . $category->name . ' - ' . $category->slug . '<br>';
 
-                        ?>
-                            <li><a href="http://wordpress.local/index.php/category/<?php echo $category->slug ?>"><i class="fa fa-angle-double-right"></i><?php echo $category->name ?></a></li>
-                        <?php
-                        }
-                        ?>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
-					</ul>
-				</div>
+					<?php
+					foreach ($categories as $category) {
+						// echo $category->term_id . ' - ' . $category->name . ' - ' . $category->slug . '<br>';
+
+					?>
+						<li><a href="http://wordpress.local/index.php/category/<?php echo $category->slug ?>"><i class="fa fa-angle-double-right"></i><?php echo $category->name ?></a></li>
+					<?php
+					}
+					?>
+				</ul>
 			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-					<ul class="list-unstyled list-inline social text-center">
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
-					</ul>
-				</div>
-				<hr>
-			</div>	
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
-				</div>
-				<hr>
-			</div>	
+			<div class="col-xs-12 col-sm-4 col-md-4">
+				<h5>Quick links</h5>
+				<ul class="list-unstyled quick-links">
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4">
+				<h5>Quick links</h5>
+				<ul class="list-unstyled quick-links">
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+					<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+					<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+				</ul>
+			</div>
 		</div>
-	</section>
-	<!-- ./Footer -->
-     <style>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+				<ul class="list-unstyled list-inline social text-center">
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+				<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+				<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ./Footer -->
+<!-- <style>
         /* Footer */
 @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 section {
@@ -131,6 +130,7 @@ section .section-title {
     border-left: 3px solid #eeeeee;
     padding-bottom: 6px;
     margin-bottom: 20px;
+    margin-left: 45px;
     color:#ffffff;
 }
 #footer a {
@@ -187,4 +187,4 @@ section .section-title {
 }
 }
 
-     </style>
+     </style> -->
